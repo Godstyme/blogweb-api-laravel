4764ced3c55e9d29a3549ddc88ce61b3363a4530
“Godstime <godstimeonyibe@gmail.com>
2022-09-27 09:05:14 +0100

    added make blog post functionality

--- app/Http/Controllers/BlogPostController.php
+++ app/Http/Controllers/BlogPostController.php
@@ -2,9 +2,112 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BlogPost;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class BlogPostController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+            $id = Auth::user()->id;
+            $validateData = [
+                'blog_title' =>   'required|string|between:5,50',
+                'blog_description' =>   'required|string|between:5,1000'
+            ];
+            $validator = Validator::make($request->all(),$validateData);
+            if ($validator->fails()) {
+                $response =  response()->json([
+                    "status"=>false,
+                    "message" =>$validator->errors()
+                ],422);
+            } else {
+                $validateUser = new BlogPost;
+                $validateUser->blog_posts_title = $request->blog_title;
+                $validateUser->blog_posts_desc = $request->blog_description;
+                $validateUser->users_id = $id;
+                $result = $validateUser->save();
+                if ($result) {
+                    $response = response()->json([
+                        "status"=>true,
+                        "message" => 'You have successful made a post'
+                    ],200);
+                } else {
+                    $response = response()->json([
+                        "status"=>false,
+                        "message" => 'Operation failed, Post content was not inserted'
+                    ],400);
+                }
+            }
+        } catch (\Throwable $th) {
+            $response = response()->json([
+                'status' => false,
+                'error' => $th->getMessage()
+            ], 500);
+        }
+        return $response;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserRegistrationController;
 use App\Http\Controllers\UserLoginController;
+use App\Http\Controllers\BlogPostController;
 use App\Http\Controllers\BloggerOperation;
 /*
 |--------------------------------------------------------------------------
@@ -21,27 +22,6 @@ use App\Http\Controllers\BloggerOperation;
 // Route::middleware('jwt.auth')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-
-
-Route::controller(UserRegistrationController::class)->group(function() {
-    Route::get('index','index');
-    Route::post('register','store');
-    Route::get('user/verify/{verification_code}', 'verifyUser');
-});
-
-Route::controller(AdminController::class)->group(function() {
-    Route::get('admin/view','index');
-    Route::post('admin/register','store');
-});
-
-// Route::post('admin/register',[AdminController::class,'store'])->middleware('admin');
-// Route::get('admin/view',[AdminController::class,'index'])->middleware('admin');
-
-Route::controller(UserLoginController::class)->group(function() {
-    Route::post('login','login');
-    Route::post('refresh', 'refresh');
-});
-
 Route::group(['middleware' => ['jwt.auth']], function() {
     Route::get('logout', [UserLoginController::class,'logout']);
 
@@ -49,6 +29,37 @@ Route::group(['middleware' => ['jwt.auth']], function() {
         Route::get('show/{id}','show');
     });
 
+    Route::controller(BlogPostController::class)->group(function() {
+        // Route::get('admin/view','index');
+        Route::post('makepost','store');
+    });
+
+
+
 });
+
+// Route about guest account creation
+Route::controller(UserRegistrationController::class)->group(function() {
+    Route::get('index','index');
+    Route::post('register','store');
+    Route::get('user/verify/{verification_code}', 'verifyUser');
+});
+
+// Route about all users account login
+Route::controller(UserLoginController::class)->group(function() {
+    Route::post('login','login');
+    Route::post('refresh', 'refresh');
+});
+
+// Route about admin account creation
+Route::controller(AdminController::class)->group(function() {
+    Route::get('admin/view','index');
+    Route::post('admin/register','store');
+});
+
+
+
+
+
 
 
